38cf4659f44eb2b79e2d9af7c2b3287d9a58c8ff
jschang19 <wwwsean13579@gmail.com>
2023-11-10 13:00:39 +0800

    refactor: Change utils path into server

--- server/utils/mysql/shop/index.ts
+++ server/utils/mysql/shop/index.ts
@@ -1,18 +1,19 @@
-import initConnection from "@/utils/mysql/connection";
+import initConnection from "@/server/utils/mysql/connection";
+import { type RowDataPacket } from "mysql2/promise";
 
 const ShopService: {
 	getByName: (
 		name: string,
-		current: number,
-		limit: number,
+		offset: number,
+		pageSize: number,
 	) => Promise<{
 		total: number;
 		rows: any[] | any;
 	}>;
 	getByCity: (
 		city: string,
-		current: number,
-		limit: number,
+		offset: number,
+		pageSize: number,
 	) => Promise<{
 		total: number;
 		rows: any[] | any;
@@ -20,27 +21,27 @@ const ShopService: {
 	getByCityAndName: (
 		city: string,
 		name: string,
-		current: number,
-		limit: number,
+		offset: number,
+		pageSize: number,
 	) => Promise<{
 		total: number;
 		rows: any[] | any;
 	}>;
 } = {
-	getByName: async (name: string, current: number, limit: number) => {
+	getByName: async (name: string, offset: number, pageSize: number) => {
 		const connection = await initConnection();
 		const likeTerm = `%${name}%`;
 		const [[total], [rows]] = await Promise.all([
-			connection.query(
+			connection.query<RowDataPacket[]>(
 				"SELECT COUNT(*) AS total FROM shop AS s JOIN shopcity AS sc ON s.shopid = sc.shopid JOIN city AS c ON c.cityid = sc.cityid  WHERE `ShopName` LIKE ?",
 				[likeTerm],
 			),
-			connection.query(
+			connection.query<RowDataPacket[]>(
 				"SELECT * FROM shop AS s JOIN shopcity AS sc ON s.shopid = sc.shopid JOIN city AS c ON c.cityid = sc.cityid  WHERE `ShopName` LIKE ? ORDER BY s.shopid LIMIT ?, ?",
-				[likeTerm, current, limit],
+				[likeTerm, offset, pageSize],
 			),
 		]);
-		const totalRows = ((total as any)[0] as any).total;
+		const totalRows = total[0].total;
 		await connection.end();
 		return {
 			total: totalRows,
@@ -48,26 +49,20 @@ const ShopService: {
 		};
 	},
 
-	getByCity: async (city: string, current: number, limit: number) => {
+	getByCity: async (city: string, offset: number, pageSize: number) => {
 		const connection = await initConnection();
-		// const [rows] = await connection.query(
-		// 	"SELECT * FROM `shopcity` AS sc JOIN shop AS s on s.ShopID = sc.ShopId JOIN city AS c ON c.cityid = sc.cityid WHERE sc.CityID = ? LIMIT ?, ?",
-		// 	[city, current, limit],
-		// );
-		// return rows;
-
 		const [[total], [rows]] = await Promise.all([
-			connection.query(
+			connection.query<RowDataPacket[]>(
 				"SELECT COUNT(*) AS total FROM shopcity AS sc JOIN shop AS s ON s.shopid = sc.ShopId JOIN city AS c ON c.cityid = sc.cityid WHERE sc.CityID = ?",
 				[city],
 			),
-			connection.query(
+			connection.query<RowDataPacket[]>(
 				"SELECT * FROM shopcity AS sc JOIN shop AS s ON s.shopid = sc.ShopId JOIN city AS c ON c.cityid = sc.cityid WHERE sc.CityID = ? ORDER BY s.shopid LIMIT ?, ?",
-				[city, current, limit],
+				[city, offset, pageSize],
 			),
 		]);
 
-		const totalRows = ((total as any)[0] as any).total;
+		const totalRows = total[0].total;
 		await connection.end();
 		return {
 			total: totalRows,
@@ -75,24 +70,24 @@ const ShopService: {
 		};
 	},
 
-	getByCityAndName: async (city: string, name: string, current: number, limit: number) => {
+	getByCityAndName: async (city: string, name: string, offset: number, pageSize: number) => {
 		const connection = await initConnection();
 		const likeTerm = `%${name}%`;
 
 		// promise all
 		const [[total], [rows]] = await Promise.all([
-			connection.query(
+			connection.query<RowDataPacket[]>(
 				"SELECT COUNT(*) AS total FROM shopcity AS sc JOIN shop AS s ON s.shopid = sc.ShopId JOIN city AS c ON c.cityid = sc.cityid WHERE sc.CityID = ? AND s.ShopName LIKE ?",
 				[city, likeTerm],
 			),
-			connection.query(
+			connection.query<RowDataPacket[]>(
 				"SELECT * FROM shopcity AS sc JOIN shop AS s ON s.shopid = sc.ShopId JOIN city AS c ON c.cityid = sc.cityid WHERE sc.CityID = ? AND s.ShopName LIKE ? LIMIT ?, ?",
-				[city, likeTerm, current, limit],
+				[city, likeTerm, offset, pageSize],
 			),
 		]);
 		await connection.end();
 		return {
-			total: ((total as any)[0] as any).total,
+			total: total[0].total,
 			rows: rows,
 		};
 	},
