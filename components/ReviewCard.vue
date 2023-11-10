d8b4d1a09b394580b05a82d857372490e06ed6ee
jschang19 <wwwsean13579@gmail.com>
2023-11-10 19:54:35 +0800

    feat: Create review result page

--- components/ReviewCard.vue
+++ components/ReviewCard.vue
@@ -1,29 +1,64 @@
 <script setup lang="ts">
 interface Review {
 	id: string;
-	shopId: string;
+	content: string;
 	author: string;
-	rating: number;
-	comment: string;
+	shopId: string;
+	averagePrice: number;
+	scores: {
+		taste: number;
+		environment: number;
+		service: number;
+		average: number;
+	};
+	createdAt: string;
 }
 
-const props = defineProps({
+const prop = defineProps({
 	review: {
 		type: Object as PropType<Review>,
 		required: true,
 	},
 });
+const tooLong = ref(prop.review.content.length > 100);
+const displayMore = ref(false);
 </script>
 
 <template>
-	<UCard class="w-full">
-		<div class="flex flex-col gap-2">
-			<p>{{ props.review.author }}</p>
-			<div class="flex flex-row gap-3 justify-start text-sm">
-				<p>食物評價</p>
-				<p>環境評價</p>
-				<p>服務評價</p>
+	<div>
+		<div class="flex flex-col gap-2 pt-1 pb-3 border-b dark:border-slate-700">
+			<p class="text-sm text-gray-700 dark:text-gray-400">{{ prop.review.author }}</p>
+			<div class="flex flex-row">
+				<UIcon
+					v-for="star in prop.review.scores.average"
+					:key="star"
+					name="i-heroicons-star-solid"
+					class="text-yellow-400"
+				/>
 			</div>
+			<p class="text-sm break-words leading-6 text-gray-700 dark:text-gray-400">
+				{{
+					tooLong
+						? !displayMore
+							? prop.review.content.slice(0, 100) + " ..."
+							: prop.review.content
+						: prop.review.content
+				}}
+				<span
+					class="underline underline-offset-2 dark:text-gray-300"
+					v-if="tooLong && !displayMore"
+					@click="displayMore = true"
+					>顯示更多</span
+				>
+			</p>
+			<p
+				class="text-sm underline underline-offset-2dark:text-gray-300"
+				v-if="tooLong && displayMore"
+				@click="displayMore = false"
+			>
+				隱藏
+			</p>
+			<p class="text-sm text-gray-700 dark:text-gray-400">評價日期：{{ prop.review.createdAt }}</p>
 		</div>
-	</UCard>
+	</div>
 </template>
