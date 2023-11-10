00bdb12488438443e5e04e3617ee9c61d1470c75
jschang19 <wwwsean13579@gmail.com>
2023-11-10 19:57:40 +0800

    feat: Create review api

--- server/api/review/[shopid].ts
+++ server/api/review/[shopid].ts
@@ -1,4 +1,58 @@
 import ReviewService from "@/server/utils/mysql/review";
+
 export default eventHandler(async (event) => {
 	const { shopid: shopId } = event.context.params as { shopid: string };
+	const { current, pageSize, orderBy } = await readBody(event);
+
+	if (!shopId) {
+		setResponseStatus(event, 400);
+		await send(event, "Bad Request");
+		return;
+	}
+
+	// check if current and pageSize are numbers
+	if (typeof current !== "number" || typeof pageSize !== "number") {
+		setResponseStatus(event, 400);
+		await send(event, "Bad Request");
+		return;
+	}
+
+	const results = await ReviewService.getByShopId(shopId, current, pageSize, "CommentID");
+	setResponseStatus(event, 200);
+	setResponseHeader(event, "Content-Type", "application/json");
+	await send(
+		event,
+		JSON.stringify({
+			total: results.total,
+			reviews: parseReviewResults(results.rows),
+		}),
+	);
 });
+
+function parseReviewResults(results: any[]) {
+	return results.map((result: any) => {
+		const average = Math.round((result.CommentTaste + result.CommentEnvironment + result.CommentService) / 3);
+		return {
+			id: result.CommentID,
+			shopId: result.ShopID,
+			author: result.Author,
+			scores: {
+				taste: result.CommentTaste,
+				environment: result.CommentEnvironment,
+				service: result.CommentService,
+				average,
+			},
+			content: result.Review,
+			averagePrice: result.AvgPrice,
+			createdAt: parseDate(result.CommentDate),
+		};
+	});
+}
+function parseDate(date: string) {
+	// yyyy-mm-dd
+	const dateObj = new Date(date);
+	const year = dateObj.getFullYear();
+	const month = dateObj.getMonth() + 1;
+	const day = dateObj.getDate();
+	return `${year}-${month}-${day}`;
+}
--- server/utils/mysql/review/index.ts
+++ server/utils/mysql/review/index.ts
@@ -1,4 +1,48 @@
 import initConnection from "@/server/utils/mysql/connection";
 import { type RowDataPacket } from "mysql2/promise";
 
-//function get
+interface ReviewService {
+	getByShopId: (
+		shopId: string,
+		offset: number,
+		pageSize: number,
+		orderBy: string,
+	) => Promise<{
+		total: number;
+		rows: any[] | any;
+	}>;
+}
+
+const ReviewService: ReviewService = {
+	getByShopId: async (shopId: string, offset: number, pageSize: number, orderBy: string) => {
+		const connection = await initConnection();
+
+		switch (orderBy) {
+			case "score":
+				orderBy = "CommentTaste DESC, CommentEnvironment DESC, CommentService DESC";
+				break;
+			case "date":
+				orderBy = "CommentDate DESC";
+				break;
+			default:
+				orderBy = "CommentDate DESC";
+				break;
+		}
+
+		const [[total], [rows]] = await Promise.all([
+			connection.query<RowDataPacket[]>("SELECT COUNT(*) AS total FROM shopcomment WHERE ShopID = ?", [shopId]),
+			connection.query<RowDataPacket[]>(`SELECT * FROM shopcomment WHERE ShopID = ? ORDER BY ${orderBy} LIMIT ?, ?`, [
+				shopId,
+				offset,
+				pageSize,
+			]),
+		]);
+		const totalRows = total[0].total;
+		return {
+			total: totalRows,
+			rows: rows,
+		};
+	},
+};
+
+export default ReviewService;
