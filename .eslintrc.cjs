26f076d1951b7b8531606392c669ddf3ef68e86b
jschang19 <wwwsean13579@gmail.com>
2023-11-14 14:23:05 +0800

    refactor: Add ignoring console.log rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,25 @@ module.exports = {
 		browser: true,
 		node: true,
 	},
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
 	parser: "vue-eslint-parser",
 	parserOptions: {
 		parser: "@typescript-eslint/parser",
 	},
+	ignorePatterns: ["node_modules", "*.config.*"],
 	extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
 	plugins: [],
-	rules: {},
+	rules: {
+		"no-console": "off",
+	},
 };
